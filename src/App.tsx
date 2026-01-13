dc5389fd0c2fe1f81c8747e97407b6e7f6f06c98
Akshay Saambram <akshay.saambram@isowhiz.com>
2026-01-13 10:54:28 +0530

    chore: initialize finance calculator project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen grid place-items-center">
+      <h1 className="text-xl font-semibold">Finance Calculator</h1>
+    </div>
+  );
+}
+
+export default App;
